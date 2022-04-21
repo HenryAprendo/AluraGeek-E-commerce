28fdc8b5b23993c638456cfd2bcf5a3289a8f258
Henry Salazar <hjsmipuc@gmail.com>
2022-04-21 12:22:03 -0500

    Mejoras en el codigo de validación

--- JS/validacionFormularioContacto.js
+++ JS/validacionFormularioContacto.js
@@ -1,3 +1,5 @@
+/*Declaración de variables generales*/
+
 const formulario = document.querySelector('[data-formulario]');
 const input = document.querySelector('[data-nombre]');
 const mensaje = document.querySelector('[data-mensaje]');
@@ -7,6 +9,15 @@ const test = {
     campoMensaje: false
 };
 
+const mensajes = {
+    limpiar: '',
+    campoVacio: ['Completa el campo nombre', 'Completa el campo mensaje'],
+    expresionInvalida: 'Solo letras mayúsculas, minúsculas y espacios. Maximo 40 carácteres',
+    envioExitoso: 'Su mensaje ha sido enviado con éxito',
+    formularioIncompleto: 'Diligencie el formulario completamente',
+}
+
+/*Funciones secundarias */
 function addClass(elemento, clase) {
     elemento.classList.add(clase);
 }
@@ -15,49 +26,49 @@ function removeClass(elemento, clase) {
     elemento.classList.remove(clase);
 }
 
+function visualizarMensaje(elemento, texto) {
+    elemento.innerHTML = texto;
+}
 
 
-
-
+/*Funciones principales */
 const validarNombre = (valor) => {
 
     const error = document.querySelector('[data-input-error]');
-    error.innerHTML = '';
+    visualizarMensaje(error, mensajes.limpiar);
 
     const formulario = document.querySelector('[data-formulario]');
     const clase = 'contacto__formulario-error-input';
 
     if (valor.validity.valueMissing) {
         addClass(formulario, clase);
-        error.innerHTML = 'Completa el campo nombre'; 
+        visualizarMensaje(error, mensajes.campoVacio[0]);
     }
     else if (valor.validity.patternMismatch) {
         addClass(formulario, clase);
-        error.innerHTML = 'Solo letras mayúsculas, minúsculas y espacios. Maximo 40 carácteres'; 
-
+        visualizarMensaje(error, mensajes.expresionInvalida);
     }
     else {
-        error.innerHTML = '';
+        visualizarMensaje(error, mensajes.limpiar);
         removeClass(formulario, clase);
         test.campoNombre = true;     
     }
     
 }
 
-
 const validarMensaje = (valor) => {
     const error = document.querySelector('[data-mensaje-error]');
-    error.innerHTML = '';
+    visualizarMensaje(error, mensajes.limpiar);
 
     const formulario = document.querySelector('[data-formulario]');
     const clase = 'contacto__formulario-error-mensaje';
 
     if (valor.validity.valueMissing) {
         addClass(formulario, clase); 
-        error.innerHTML = 'Completa el campo mensaje'; 
+        visualizarMensaje(error, mensajes.campoVacio[1]);
     }
     else {
-        error.innerHTML = '';
+        visualizarMensaje(error, mensajes.limpiar);
         removeClass(formulario, clase);
         test.campoMensaje = true;
     }
@@ -67,18 +78,16 @@ const validarMensaje = (valor) => {
 const mostrarMensajesDeValidacion = () => {
     
     const mensaje = document.querySelector('[data-incompleto]');
-    mensaje.innerHTML = '';
+    visualizarMensaje(mensaje, mensajes.limpiar);
 
     const formulario = document.querySelector('[data-formulario]');
     const clase = 'formulario__completo';
-    const grupoDeClases = [ 'contacto__formulario-error-incompleto', 'contacto__formulario-error-input', 'contacto__formulario-error-mensaje' ];
 
     if (test.campoNombre && test.campoMensaje) {
 
         formulario.classList.remove('contacto__formulario-error-incompleto', 'contacto__formulario-error-input', 'contacto__formulario-error-mensaje');
-        // removeClass(formulario, grupoDeClases);
         addClass(mensaje, clase);
-        mensaje.innerHTML = 'Su mensaje ha sido enviado con éxito';
+        visualizarMensaje(mensaje, mensajes.envioExitoso);
 
         setTimeout( () => {
             removeClass(mensaje, clase);
@@ -86,19 +95,15 @@ const mostrarMensajesDeValidacion = () => {
 
         test.campoNombre = false;
         test.campoNombre = false;
-
         formulario.reset();
     }
     else {
-    
         formulario.classList.add('contacto__formulario-error-incompleto', 'contacto__formulario-error-input', 'contacto__formulario-error-mensaje');
-        mensaje.innerHTML = 'Diligencie el formulario completamente';
-
+        visualizarMensaje(mensaje, mensajes.formularioIncompleto);        
     }
 }
 
-
-
+/*Detección de eventos en los campos del formulario*/
 input.addEventListener('blur', input => {
     validarNombre(input.target)
 });
