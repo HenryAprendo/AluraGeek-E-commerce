97889a14e00ebcd666c2c24f8cd1f812858c12a9
Henry Salazar <hjsmipuc@gmail.com>
2022-05-03 13:12:35 -0500

    Codigo para limpiar el formulario y cargar una nueva imagen

--- JS/servidor/enviarProducto.js
+++ JS/servidor/enviarProducto.js
@@ -13,9 +13,10 @@ formularioAgregar.addEventListener('submit', e => {
     const descripcionProducto = document.querySelector('[data-descripcion]').value;
 
     let inputFile = document.querySelector('[data-input-file]');
+    const data = inputFile.files[0];
 
     const reader = new FileReader();
-    reader.readAsDataURL(inputFile.files[0]);
+    reader.readAsDataURL(data);
     reader.onload = e => {
 
         const imagenProducto = reader.result;
@@ -28,11 +29,40 @@ formularioAgregar.addEventListener('submit', e => {
         }
     
         enviarDatos(datos);
+        removerImagen();
     }    
 
+    
     formularioAgregar.reset();
 
 });
+
+
+const removerImagen = () => {
+
+    let zonaDropImagen = document.querySelector('.zona__drog__imagen');    
+
+    if (zonaDropImagen) {
+        zonaDropImagen.classList.remove('zona__drog__imagen');
+    }
+    
+    let zonaDropElemento = document.querySelector('.zona__drog__prompt')
+
+    if (!zonaDropElemento) {
+        const zonaDrop = document.querySelector('.zona__drog');
+        const content = ` 
+        <figure class="zona__drog__prompt ">
+            <picture>
+                <!-- <source media="(min-width: 1130px)" srcset="./img/logo/Geek.png"> -->
+                <source media="(min-width: 768px)" srcset="./img/logo/insertar.png">
+                <img src="./img/logo/add.png" alt="agregar">
+            </picture>
+            <figcaption class="zona__drog__texto" >Arrastre o haga click para agregar una imagén para el producto</figcaption>
+        </figure>`;
+
+        zonaDrop.innerHTML = content;
+    }
+}
 
 
 
