94213901e2d617af3fa0122a2e97e6a320506d45
Henry Salazar <hjsmipuc@gmail.com>
2022-05-05 09:31:41 -0500

    Se modifico el db.json por secciones y se modifico el código que visualiza los productos

--- JS/servidor/visualizarProductos.js
+++ JS/servidor/visualizarProductos.js
@@ -2,10 +2,18 @@ import { serviciosServidor } from "./services.js";
 
 /*Se encarga de crear los elementos en el DOM para desplegar cada uno de estos productos */
 const visualizarProductos = (datos) => {
-    datos.forEach( producto => {
+
+
+   const crearProducto = (seccion) => {
+    seccion.forEach(producto => {
         console.log(producto);
-        console.log(producto.nombre);
-    })
+    });
+   }
+
+   crearProducto(datos.startwars);
+   crearProducto(datos.consolas);
+   crearProducto(datos.diversos);
+
 }
 
 
