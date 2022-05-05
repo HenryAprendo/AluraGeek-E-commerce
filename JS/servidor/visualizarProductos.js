7483ce6ab807099de84a75a6c82f985cc7293db0
Henry Salazar <hjsmipuc@gmail.com>
2022-05-05 10:25:40 -0500

    Creción de DOM por cada producto , contenedor e información

--- JS/servidor/visualizarProductos.js
+++ JS/servidor/visualizarProductos.js
@@ -6,13 +6,53 @@ const visualizarProductos = (datos) => {
 
    const crearProducto = (seccion) => {
     seccion.forEach(producto => {
-        console.log(producto);
+        const nombre = producto.nombre;
+        const precio = producto.precio;
+        const urlImage = producto.imagen;
+
+        const seccion = document.querySelector('[data-test]');
+
+        const cover = document.createElement('div');
+        cover.classList.add('card');
+
+        const coverInfo = document.createElement('div');
+        coverInfo.classList.add('product__info');
+
+        const infoNombre = document.createElement('p');
+        infoNombre.classList.add('card__name');
+        infoNombre.innerHTML = nombre;
+
+        const infoPrecio= document.createElement('p');
+        infoPrecio.classList.add('card__price');
+        infoPrecio.innerHTML = `$ ${precio}`;
+
+        const infoEnlace = document.createElement('a');
+        infoEnlace.classList.add('card__enlace');
+        infoEnlace.textContent = 'Ver producto';
+
+        coverInfo.appendChild(infoNombre);
+        coverInfo.appendChild(infoPrecio);
+        coverInfo.appendChild(infoEnlace);
+
+        cover.appendChild(coverInfo);
+        seccion.appendChild(cover);
+
+
+
+        //     `<div class="card">
+        //     <img class="card_img" src="./img/start-wars/1-start-wars.png" alt="start-wars">
+        //     <div class="product__info">
+        //         <p class="card__name">Producto XYZ</p>
+        //         <p class="card__price">$ 60,00</p>
+        //         <a class="card__enlace">Ver producto</a>
+        //     </div>
+        //     </div>`
     });
    }
 
    crearProducto(datos.startwars);
-   crearProducto(datos.consolas);
-   crearProducto(datos.diversos);
+//    crearProducto(datos.consolas);
+//    crearProducto(datos.diversos);
 
 }
 
