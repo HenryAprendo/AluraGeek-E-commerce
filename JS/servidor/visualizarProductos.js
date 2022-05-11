beb3b9ab0d6a286135bab42f7fb64e6405b8e362
Henry Salazar <hjsmipuc@gmail.com>
2022-05-11 11:51:24 -0500

    cambio en la configuración del archivo db.json y en la función visualizar productos

--- JS/servidor/visualizarProductos.js
+++ JS/servidor/visualizarProductos.js
@@ -3,17 +3,37 @@ import { obtenerProductos } from "./obtenerProductos.js";
 
 /*Se encarga de crear los elementos en el DOM para desplegar cada uno de estos productos */
 const visualizarProductos = (datos) => {
-    const seccionStart = document.querySelector('[data-primer-grupo]');
-    const seccionConsola = document.querySelector('[data-segundo-grupo]');
-    const seccionDiversos = document.querySelector('[data-tercer-grupo]');
+   const seccionStart = document.querySelector('[data-primer-grupo]');
+   const seccionConsola = document.querySelector('[data-segundo-grupo]');
+   const seccionDiversos = document.querySelector('[data-tercer-grupo]');
 
-   crearProducto(datos.startwars, seccionStart);
-   crearProducto(datos.consolas, seccionConsola);
-   crearProducto(datos.diversos, seccionDiversos);
+
+   const categoriaStarwars = [];
+   const categoriaConsolas = [];
+   const categoriaDiversos = [];
+
+
+   datos.forEach( item => {
+      if (item.categoria === "star-wars") {
+         categoriaStarwars.push(item);
+      }
+      else if(item.categoria === "consolas"){
+         categoriaConsolas.push(item);
+      }
+      else {
+         categoriaDiversos.push(item);
+      }
+   });
+   
+
+   crearProducto(categoriaStarwars, seccionStart);
+   crearProducto(categoriaConsolas, seccionConsola);
+   crearProducto(categoriaDiversos, seccionDiversos);
 
 }
 
  obtenerProductos().then( respuesta => {
+    console.log(respuesta);
     visualizarProductos(respuesta);
  } )
 
