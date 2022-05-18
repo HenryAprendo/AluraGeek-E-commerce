d1c19feca37ceb382be9c654885507eca16188f3
Henry Salazar <hjsmipuc@gmail.com>
2022-05-18 10:27:48 -0500

    Estructura html para editar productos con sus respectivos estilos

--- JS/servidor/enviarProducto.js
+++ JS/servidor/enviarProducto.js
@@ -2,7 +2,6 @@ import { serviciosServidor } from "./services.js";
 
 const formularioAgregar = document.querySelector('[data-form-agregar]');
 
-
 formularioAgregar.addEventListener('submit', e => {
     e.preventDefault();
     console.log('Enviando datos al servidor');
@@ -31,38 +30,9 @@ formularioAgregar.addEventListener('submit', e => {
         serviciosServidor.crearProducto(nombreProducto, precioProducto, descripcionProducto,imagenProducto, uuid.v4());
     }    
 
-    
-    // formularioAgregar.reset();
-    // location.reload();
-
 });
+   
 
-
-// const removerImagen = () => {
-
-//     let zonaDropImagen = document.querySelector('.zona__drog__imagen');    
-
-//     if (zonaDropImagen) {
-//         zonaDropImagen.classList.remove('zona__drog__imagen');
-//     }
-    
-//     let zonaDropElemento = document.querySelector('.zona__drog__prompt')
-
-//     if (!zonaDropElemento) {
-//         const zonaDrop = document.querySelector('.zona__drog');
-//         const content = ` 
-//         <figure class="zona__drog__prompt ">
-//             <picture>
-//                 <!-- <source media="(min-width: 1130px)" srcset="./img/logo/Geek.png"> -->
-//                 <source media="(min-width: 768px)" srcset="./img/logo/insertar.png">
-//                 <img src="./img/logo/add.png" alt="agregar">
-//             </picture>
-//             <figcaption class="zona__drog__texto" >Arrastre o haga click para agregar una imagén para el producto</figcaption>
-//         </figure>`;
-
-//         zonaDrop.innerHTML = content;
-//     }
-// }
 
 
 
--- JS/servidor/mostrarTodosLosProductos.js
+++ JS/servidor/mostrarTodosLosProductos.js
@@ -18,6 +18,7 @@ const visualizarTodosLosProductos = (datos) => {
         });
     });
     
+    
 
 }
 
